aacae7f7b2ec56b596bb00a3f2baa0c5f57726c1
DJ Chao <Dchao88@djs-macbook-pro.dhcp.nd.edu>
2017-03-20 20:43:25 -0400

    6 digit prime numbers and program to find primes

---
+++ nd_coin/primes/primes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2.7
+
+import os
+import sys
+import string
+import itertools
+
+# Constants
+
+LOWER = 100001
+UPPER = 1000000
+
+def isPrime(number):
+	if number % 2 == 0:
+		return 0
+
+	for num in range(3, (number/2), 2):
+		if number % num == 0:
+			return 0
+
+	return 1;
+
+for num in range(LOWER, UPPER, 2):
+	if isPrime(num) == 1:
+		print num
