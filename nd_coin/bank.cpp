6dcef39e0a46c2f6de9b88fb2037d13404187cc5
Brenden Judson <Brenden@brendens-mbp.dhcp.nd.edu>
2017-04-30 20:32:05 -0400

    bank.cpp

--- nd_coin/bank.cpp
+++ nd_coin/bank.cpp
@@ -1,15 +1,56 @@
 #include "bank.h"
 
+
+
 bank::bank(){
+	//set coin serial numbers
 
 }
 
-void bank::setBalance(string account_name,int amount){
-	map<string,int>::iterator it = accounts.begin();
+//add coin to account
+void bank::giveCoin(string account_name,int serial_number){
+	//if the account exists
+	if(accounts.find(account_name) != accounts.end())
+		accounts[account_name].serial_numbers.push_back(serial_number);
 
+	else{
+		//else create account
+		account newAccount;
+		newAccount.serial_numbers.push_back(serial_number);
+		map<string,account>::iterator it = accounts.begin();
+		accounts.insert(it,pair<string,account>(account_name,newAccount));
+	}
+}
+
+//take coin from account
+bool bank::takeCoin(string account_name,int serial_number){
+	//if the account exists
+	if(accounts.find(account_name) != accounts.end()){
+		//search account for coin
+		for(vector<int>::iterator it=accounts[account_name].serial_numbers.begin(); it!=accounts[account_name].serial_numbers.end(); ++it){
+			//if found
+			if(*it == serial_number){
+				accounts[account_name].serial_numbers.erase(it);
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+void bank::makeCoin(){
+//	serial = rand() % 1000;
+
+//	coins.push_back();
+}
+/*void bank::setBalance(string account_name,int amount){
+	map<string,account>::iterator it = accounts.begin();
+
+	accounts.erase(account_name);
 	accounts.insert(it,pair<string,int>(account_name,amount));
-}
+}*/
 
 int bank::getBalance(string name){
-	return accounts.find(name)->second;
+	return accounts[name].serial_numbers.size();
 }
